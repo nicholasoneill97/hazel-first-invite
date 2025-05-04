53125700dbecc39c37a0fedc34484561a5ae44cb
Nicholas O'Neill <159676183+nicholasoneill97@users.noreply.github.com>
2025-05-04 16:08:01 -0400

    Add files via upload

---
+++ hazel.js
@@ -0,0 +1,18 @@
+console.log('hey')
+
+
+let image = document.getElementsByTagName('img') 
+
+
+
+const hazel = ( image ) => {
+  return (
+    <div>
+        <img src={image} className="big-pic" />
+    </div>
+  )
+}
+
+
+
+export default hazel
